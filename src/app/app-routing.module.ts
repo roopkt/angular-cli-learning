2c9c6e00bfdcffe4faa841dfc6bfa89a4cdfb20b
roopkt <roopkt@gmail.com>
2017-05-18 12:58:09 -0400

    working on angular cli project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { DashboardComponent} from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerComponent } from "./customer/customer.component";
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    pathMatch:'full',
+    redirectTo:'dashboard'
+  },
+  {
+    path:'dashboard', component:DashboardComponent
+  },
+   {
+    path:'customer', component:CustomerComponent
   }
 ];
 
